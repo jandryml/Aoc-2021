956cd6d3cb09a962e42bef99fa0fe32d396b4874
Jan Dryml <honza.dryml@icloud.com>
2021-12-01 14:26:43 +0100

    Day 1 - refactor

--- src/kotlin/org/aoc/Day01.kt
+++ src/kotlin/org/aoc/Day01.kt
@@ -1,57 +1,27 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        var count = 1
-        for (i in (0 until input.lastIndex)) {
-            if (input[i] < input[i + 1]) {
-                count++
-            }
-        }
-        return count
+        return input.map { it.toInt() }
+            .filterIndexed { i, value -> if (i != input.lastIndex) value < input[i + 1].toInt() else false }
+            .count()
     }
 
-    fun addValueToIndices(indices: List<Int>, value: Int, array: IntArray) {
-        indices.forEach {
-            array[it] += value
-        }
-    }
-
-    fun areValuesIncreasing(indexA: Int, indexB: Int, array: IntArray): Boolean {
-        return (array[indexA] < array[indexB])
+    fun handleSlidingWindows(indexA: Int, indexB: Int, inputValue: Int, actualIndex: Int, array: IntArray): Boolean {
+        listOf(0, 1, 2, 3).filterNot { it == indexA }.onEach { array[it] += inputValue }
+        val returnValue = (actualIndex > 2 && array[indexA] < array[indexB])
+        array[indexA] = 0
+        return returnValue
     }
 
     fun part2(input: List<String>): Int {
         var count = 0
-        val windows = IntArray(4)
+        val slidingWindows = IntArray(4)
 
         for (i in (0..input.lastIndex)) {
-            val value = input[i].toInt()
-
             when (i % 4) {
-                0 -> {
-                    windows[0] = 0
-                    addValueToIndices(listOf(0, 2, 3), value, windows)
-                }
-                1 -> {
-                    windows[1] = 0
-                    addValueToIndices(listOf(0, 1, 3), value, windows)
-                }
-                2 -> {
-                    windows[2] = 0
-                    addValueToIndices(listOf(0, 1, 2), value, windows)
-                }
-                3 -> {
-                    windows[3] = 0
-                    addValueToIndices(listOf(1, 2, 3), value, windows)
-                }
-            }
-
-            if (i > 2) {
-                when (i % 4) {
-                    3 -> if (areValuesIncreasing(0, 1, windows)) count++
-                    0 -> if (areValuesIncreasing(1, 2, windows)) count++
-                    1 -> if (areValuesIncreasing(2, 3, windows)) count++
-                    2 -> if (areValuesIncreasing(3, 0, windows)) count++
-                }
+                0 -> if (handleSlidingWindows(1, 2, input[i].toInt(), i, slidingWindows)) count++
+                1 -> if (handleSlidingWindows(2, 3, input[i].toInt(), i, slidingWindows)) count++
+                2 -> if (handleSlidingWindows(3, 0, input[i].toInt(), i, slidingWindows)) count++
+                3 -> if (handleSlidingWindows(0, 1, input[i].toInt(), i, slidingWindows)) count++
             }
         }
         return count
