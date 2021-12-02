c1df8775fe31b4f431ad5d4ebbc9df4073e057f7
Jan Dryml <honza.dryml@icloud.com>
2021-12-02 22:03:48 +0100

    Day 2

---
+++ src/kotlin/org/aoc/Day02.kt
@@ -0,0 +1,51 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        var depth = 0
+        var horizon = 0
+
+        input.forEach {
+            val parsed = it.split(' ')
+            when (parsed[0]) {
+                "forward" -> horizon += parsed[1].toInt()
+                "down" -> depth += parsed[1].toInt()
+                "up" -> depth -= parsed[1].toInt()
+                else -> println("invalid ${parsed[0]}")
+
+            }
+        }
+
+        return depth * horizon
+    }
+
+
+    fun part2(input: List<String>): Int {
+        var depth = 0
+        var horizon = 0
+        var aim = 0
+
+        input.forEach {
+            val parsed = it.split(' ')
+            val value = parsed[1].toInt()
+            when (parsed[0]) {
+                "forward" -> {
+                    horizon += value
+                    depth += aim * value
+                }
+                "down" -> aim += value
+                "up" -> aim -= value
+                else -> println("invalid ${parsed[0]}")
+            }
+        }
+
+        return depth * horizon
+    }
+
+
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
--- src/kotlin/org/aoc/Utils.kt
+++ src/kotlin/org/aoc/Utils.kt
@@ -5,7 +5,7 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src/resources/day1", "$name.txt").readLines()
+fun readInput(name: String) = File("src/resources/files/", "$name.txt").readLines()
 
 /**
  * Converts string to md5 hash.
