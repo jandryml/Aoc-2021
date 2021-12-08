6704ffe4ef3da0a1d612be3f4c246317df6a6d8f
Jan Dryml <honza.dryml@icloud.com>
2021-12-08 01:44:01 +0100

    Template

---
+++ src/kotlin/org/aoc/Template.kt
@@ -0,0 +1,25 @@
+package org.aoc
+
+import readInput
+import kotlin.math.abs
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+
+    val testInput = readInput("Day<changeme>_test")
+    check(part1(testInput) == 0)
+    check(part2(testInput) == 0)
+
+    val input = readInput("Day<changeme>")
+    println(part1(input))
+    println(part2(input))
+}
